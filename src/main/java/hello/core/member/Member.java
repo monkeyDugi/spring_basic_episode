9beb1c75614dd106f9d6228c53127ee88cc71b29
monkeyDugi <lbd4946@gmail.com>
2022-01-10 12:33:10 +0900

    feat : 순수 자바 -> 회원 가입 하기

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,53 @@
+package hello.core.member;
+
+import java.util.Objects;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(getId(), member.getId()) && Objects.equals(getName(), member.getName()) && getGrade() == member.getGrade();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getGrade());
+    }
+}
