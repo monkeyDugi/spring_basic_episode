5f801ad6b8f61f79a02970634e824f949297f3b1
monkeyDugi <lbd4946@gmail.com>
2022-01-11 10:25:46 +0900

    feat : BeanDefinition의 메타 정보

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,10 +1,11 @@
 package hello.core.xml;
 
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class XmlAppContext {
 
@@ -12,6 +13,6 @@ class XmlAppContext {
     void xmlAppContext() {
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
         MemberService memberService = ac.getBean("memberService", MemberService.class);
-        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
