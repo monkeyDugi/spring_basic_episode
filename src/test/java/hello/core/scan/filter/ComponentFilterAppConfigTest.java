7d1846f28478d80bd0fc5497f9cfa004d2fefdf6
monkeyDugi <lbd4946@gmail.com>
2022-01-11 19:37:00 +0900

    chore

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -1,6 +1,5 @@
 package hello.core.scan.filter;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
@@ -9,8 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.context.annotation.ComponentScan.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.context.annotation.ComponentScan.Filter;
 
 class ComponentFilterAppConfigTest {
 
