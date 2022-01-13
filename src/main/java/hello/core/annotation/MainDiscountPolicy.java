8f568800f36eca4ca9ec36e732af40fa8a40f75a
monkeyDugi <lbd4946@gmail.com>
2022-01-13 10:35:18 +0900

    feat : 조회한 빈이 모두 필요할 때, List, Map

---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+}
--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,8 +1,12 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@MainDiscountPolicy
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private final int discountRate = 10;
