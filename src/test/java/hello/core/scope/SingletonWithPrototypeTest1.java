d92f44e9b52f53c8f8684482482f257549ccd1ee
monkeyDugi <lbd4946@gmail.com>
2022-01-14 10:40:48 +0900

    feat: 프로토타입 프로바이더

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -34,20 +35,19 @@ class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean1.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     static class ClientBean {
 
-        // 생성 시점에 주입이 되어있기 때문에 다시 요청을 한다고 해도 동일한 빈이 사용된다. 즉, 싱글톤 마냥 사용되는 것이다.
-        // 하지만 매번 새로운 프로토 타입을 주입 받고 싶은게 목적이다. 어떻게 해야하
-        private final PrototypeBean prototypeBean;
+        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
